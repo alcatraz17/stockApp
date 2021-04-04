53ad02b211bf1c10957860c3b2c07a8861bd16f3
Naved Anjum <naved1504@gmail.com>
2021-04-04 23:09:04 +0530

    Added heroku links in front-end as well as improved codes

--- backend/src/routes/search.js
+++ backend/src/routes/search.js
@@ -1,23 +1,27 @@
 const Stock = require("../model/stock");
 
 const search = async (req, res) => {
-  const { name = "", page, limit } = req.query;
-  const options = {
-    page: page || 1,
-    limit: limit || 10,
-  };
-  const query = [
-    {
-      $match: {
-        name: { $regex: `${name}`, $options: "i" },
+  try {
+    const { name = "", page, limit } = req.query;
+    const options = {
+      page: page || 1,
+      limit: limit || 10,
+    };
+    const query = [
+      {
+        $match: {
+          name: { $regex: `${name}`, $options: "i" },
+        },
       },
-    },
-  ];
+    ];
 
-  const aggregate = Stock.aggregate(query);
-  const data = await Stock.aggregatePaginate(aggregate, options);
-  // console.log(data);
-  res.status(200).send(data);
+    const aggregate = Stock.aggregate(query);
+    const data = await Stock.aggregatePaginate(aggregate, options);
+    // console.log(data);
+    res.status(200).send(data);
+  } catch (e) {
+    res.status(500).send(e.stack);
+  }
 };
 
 module.exports = search;
